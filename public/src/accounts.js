f4e9e1af4b279da555279dc0b1c14c7f0b0539ca
Federico Buzeta Borquez <56175186+Fbuzeta11@users.noreply.github.com>
2023-11-20 10:16:02 -0500

    accounts test 1

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,5 +1,7 @@
+//1) should return the account object when given a particular ID
 function findAccountById(accounts, id) {}
-
+let found = accounts.find((account) => account.id === accounts.id);
+return found;
 function sortAccountsByLastName(accounts) {}
 
 function getTotalNumberOfBorrows(account, books) {}
