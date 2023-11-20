745973e6653e4b057ca9030109e6ff3fada78e4e
Federico Buzeta Borquez <56175186+Fbuzeta11@users.noreply.github.com>
2023-11-20 13:12:43 -0500

    accounts tests 2

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -6,17 +6,16 @@ function findAccountById(accounts, id) {
 
 //2) should return the list of accounts ordered by last name
 function sortAccountsByLastName(accounts) {
-accounts.sort((accountA, accountB) => {
-accountA.name.last.toLowerCase() > accountB.name.last.toLowerCase() ? 1 : -1
-);
-return accounts;
+  accounts.sort((accountA, accountB) =>
+    accountA.name.last.toLowerCase() > accountB.name.last.toLowerCase() ? 1 : -1
+  );
+  return accounts;
 }
-
 
 //3) should return the number of times an account has created a 'borrow' record
 function getTotalNumberOfBorrows(account, books) {
   let result = 0;
-  for (let i = 0; i < books.length; i++){
+  for (let i = 0; i < books.length; i++) {
     for (let b = 0; b < books[i].borrows.length; b++) {
       if (account.id === books[i].borrows[b].id) {
         result += 1;
@@ -26,22 +25,18 @@ function getTotalNumberOfBorrows(account, books) {
   return result;
 }
 
-
 //4) should return all of the books taken out by an account with the author embedde
 function getBooksPossessedByAccount(account, books, authors) {
-   
-   return (
-    books
-      
-      .filter(
-        (book) => book.borrows[0].id === account.id && !book.borrows[0].returned
-      )
-      
-      .map((book) => {
-        book["author"] = authors.find((author) => author.id === book.authorId);
-        return book;
-      })
-  );
+  return books
+
+    .filter(
+      (book) => book.borrows[0].id === account.id && !book.borrows[0].returned
+    )
+
+    .map((book) => {
+      book["author"] = authors.find((author) => author.id === book.authorId);
+      return book;
+    });
 }
 
 module.exports = {
