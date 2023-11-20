9fd7cc4242905de39fd18fac0efa615c173f3776
Federico Buzeta Borquez <56175186+Fbuzeta11@users.noreply.github.com>
2023-11-20 16:54:24 -0500

    home test

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,38 @@
-function findAuthorById(authors, id) {}
+//should return the author object when given a particular ID
+function findAuthorById(authors, id) {
+  return (found = authors.find((author) => author.id === id));
+}
 
-function findBookById(books, id) {}
+//should return the book object when given a particular ID
+function findBookById(books, id) {
+  return (found = books.find((book) => book.id === id));
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+//should return an array with two arrays: borrowed books and returned books
+function partitionBooksByBorrowedStatus(books) {
+  let booksReturned = books.filter((book) =>
+    book.borrows.every((borrow) => borrow.returned === true)
+  );
 
-function getBorrowersForBook(book, accounts) {}
+  let booksBorrowed = books.filter((book) =>
+    book.borrows.some((borrow) => borrow.returned === false)
+  );
+
+  let finalArray = [[...booksBorrowed], [...booksReturned]];
+  return finalArray;
+}
+
+//should return an array for a book of all borrowers with their information and return status
+//should limit the list to ten borrowers
+function getBorrowersForBook(book, accounts) {
+  return book.borrows
+    .map((borrow) => {
+      let account = accounts.find((account) => account.id === borrow.id);
+
+      return { ...borrow, ...account };
+    })
+    .slice(0, 10);
+}
 
 module.exports = {
   findAuthorById,
--- public/src/home.js
+++ public/src/home.js
@@ -1,14 +1,73 @@
-function getTotalBooksCount(books) {}
+//should return the total number of books in the array
+function getTotalBooksCount(books) {
+  return books.length;
+}
 
-function getTotalAccountsCount(accounts) {}
+//should return the total number of accounts in the array
+//should return zero if the array is empty
+function getTotalAccountsCount(accounts) {
+  return accounts.length;
+}
 
-function getBooksBorrowedCount(books) {}
+//should return the total number of books that are currently borrowed
+function getBooksBorrowedCount(books) {
+  let bookborrowed = books.filter(
+    (book) => book.borrows.filter((list) => list.returned === false).length > 0
+  );
+  return bookborrowed.length;
+}
 
-function getMostCommonGenres(books) {}
+//should return an ordered list of most common genres
+//should limit the list to the top five
+function getMostCommonGenres(books) {
+  let map = {};
+  books.forEach((num) => {
+    if (map[num.genre]) {
+      map[num.genre]++;
+    } else {
+      map[num.genre] = 1;
+    }
+  });
+  return Object.entries(map)
+    .map(([name, count]) => {
+      return {
+        name,
+        count,
+      };
+    })
+    .sort((a, b) => b.count - a.count)
+    .slice(0, 5);
+}
 
-function getMostPopularBooks(books) {}
+//should return an ordered list of most popular books
+//should limit the list to the top five
+function getMostPopularBooks(books) {
+  return books
+    .map((book) => {
+      return { name: book.title, count: book.borrows.length };
+    })
+    .sort((a, b) => (a.count < b.count ? 1 : -1))
+    .slice(0, 5);
+}
 
-function getMostPopularAuthors(books, authors) {}
+//should return an ordered list of most popular authors
+//should limit the list to the top five
+function getMostPopularAuthors(books, authors) {
+  let result = [];
+  authors.forEach((author) => {
+    let theAuthor = {
+      name: `${author.name.first} ${author.name.last}`,
+      count: 0,
+    };
+    books.forEach((book) => {
+      if (book.authorId === author.id) {
+        theAuthor.count += book.borrows.length;
+      }
+    });
+    result.push(theAuthor);
+  });
+  return result.sort((a, b) => b.count - a.count).slice(0, 5);
+}
 
 module.exports = {
   getTotalBooksCount,
